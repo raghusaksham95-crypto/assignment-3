57e7673f7fa7da15f8a23f85718e08d45b5d277c
Saksham Raghuvanshi <raghusaksham95@gmail.com>
2025-12-03 20:01:31 +0530

    Add files via upload

---
+++ README.c
@@ -0,0 +1,19 @@
+Rock-Paper-Scissors Game in C
+
+ This is a simple Rock-Paper-Scissors game where player play against the computer.
+ The computer randomly chooses one of the three moves, And player will Try to beat it.
+ 
+
+ Rules of the game-(Rock beats Scissors,Paper beats Rock,Scissors beats Paper)
+                     1 is referred as Rock
+                     2 is referred as Paperr
+                     3 is referred as Scissors
+					 (If both player And computer choose the same move, it is a draw.
+					 The player can Continue playing after 4 rounds Or choose to quit the game.)
+
+Controls:Enter 1, 2, Or 3 to select your move.
+         After 4 rounds, press y to Continue Or n to exit the game.
+
+Features: (a)it is a simple console-based game.
+          (b)This game allows continuous play until the user decides to quit.
+          (c)This game displays the winner of each round clearly.
